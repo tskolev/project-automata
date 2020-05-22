13b941446306e82aa1e1c37837b18303bdea83bc
Tsvetomir Kolev <tskolev.contact@gmail.com>
2020-05-22 20:41:59 +0300

    adding base files

---
+++ automaton/DeterminateFiniteAutomatonChar.h
@@ -0,0 +1,18 @@
+//
+// Created by Tsvetomir Kolev on 22-May-20.
+//
+
+#ifndef AUTOMATON_DETERMINATEFINITEAUTOMATONCHAR_H
+#define AUTOMATON_DETERMINATEFINITEAUTOMATONCHAR_H
+
+
+#include "DeterminateFiniteAutomaton.h"
+
+class DeterminateFiniteAutomatonChar : public DeterminateFiniteAutomaton {
+
+private:
+    char *alphabetChar;
+};
+
+
+#endif //AUTOMATON_DETERMINATEFINITEAUTOMATONCHAR_H
